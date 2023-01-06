88e019629e22eeffc0dba6383908dc4f19112494
Eshan Singh <eshansingh@gmail.com>
2023-01-06 11:30:31 -0500

    Remove Chapters 2 and 4
    Change content in former Chapter 3 to make it work as Chapter 2 with no
    dependency on hashing
    
    Co-authored-by: m.castorina93@gmail.com

--- src/0.2-logging/src/main.rs
+++ src/0.2-logging/src/main.rs
@@ -5,8 +5,6 @@ use std::{env, io};
 
 mod utils;
 
-use sha2::{Digest, Sha256};
-
 // Akin to the hidden .git directory, this is the directory where rat will store
 // the history of the nest. The real .git directory is a bit more complicated
 // than we're going to make it, but the concept is the same - everything that
@@ -71,9 +69,9 @@ fn main() -> Result<(), Box<dyn Error>> {
                 Err("Cancelled commit.")?;
             }
 
-            let hash = commit(&message)?;
+            let number = commit(&message)?;
 
-            format!("Created commit number {hash}.")
+            format!("Created commit number {number}.")
         }
         "log" => log()?,
         _ => Err("Invalid subcommand.")?,
@@ -89,44 +87,35 @@ fn main() -> Result<(), Box<dyn Error>> {
 /// Initializes a new rat nest in the current directory.
 fn init() -> Result<(), io::Error> {
     fs::create_dir(RAT_NEST)?;
-    fs::write(format!("{RAT_NEST}/HEAD"), "")?;
-    fs::create_dir(format!("{RAT_NEST}/commits"))?;
-    fs::create_dir(format!("{RAT_NEST}/contents"))?;
+    fs::write(format!("{RAT_NEST}/HEAD"), "-1")?;
 
     Ok(())
 }
 
 /// Commits the contents of the current directory to the nest.
-fn commit(message: &str) -> Result<String, Box<dyn Error>> {
+fn commit(message: &str) -> Result<i32, Box<dyn Error>> {
     let head_file = format!("{RAT_NEST}/HEAD");
-    let working_dir = env::current_dir()?;
 
-    let current_head = fs::read_to_string(&head_file)?;
+    let head_string = fs::read_to_string(&head_file)?;
+    let head_number: i32 = head_string.parse()?;
 
-    let metadata = format!("parent {current_head}\n\n{message}");
-
-    // Create a Sha256 Hasher and use it to create a hash of the contents of
-    // each of the files in the working directory, plus the metadata.
-    let mut hasher = Sha256::new();
-    hasher.update(&metadata);
-    utils::hash_directory(&mut hasher, &working_dir, &[RAT_NEST])?;
-    let new_commit_hash = utils::encode_hash(hasher.finalize());
-
-    // Write the commit metadata, and only the metadata, to a file.
-    fs::write(format!("{RAT_NEST}/commits/{new_commit_hash}"), metadata)?;
+    let new_head_number = head_number + 1;
 
     // Create a new directory for the new commit inside the nest.
-    let commit_dir = format!("{RAT_NEST}/contents/{new_commit_hash}");
+    let commit_dir = format!("{RAT_NEST}/commit-{new_head_number}");
     fs::create_dir(&commit_dir)?;
 
     // Copy the current working directory into the commit directory, ignoring
     // the nest itself.
-    utils::copy_dir_deep(working_dir, &commit_dir, &[RAT_NEST])?;
+    utils::copy_dir_deep(env::current_dir()?, &commit_dir, &[RAT_NEST])?;
+
+    // Write a message into the .message file in that directory
+    fs::write(format!("{commit_dir}/.message"), message)?;
 
     // Update the HEAD file with the new commit that we just created.
-    fs::write(head_file, &new_commit_hash)?;
+    fs::write(head_file, new_head_number.to_string())?;
 
-    Ok(new_commit_hash)
+    Ok(new_head_number)
 }
 
 fn log() -> Result<String, Box<dyn Error>> {
@@ -134,58 +123,31 @@ fn log() -> Result<String, Box<dyn Error>> {
     // we're going to be digging into its parents and need a way to bail out
     // once we get to the root.
     let head_file = format!("{RAT_NEST}/HEAD");
-    let mut current_head = Some(fs::read_to_string(&head_file)?);
+    let current_head: i32 = fs::read_to_string(head_file)?.parse()?;
 
     // We initialize the string we're eventually going to return.
     let mut logs = String::new();
 
-    while let Some(head) = &current_head {
-        // This is the header, which is simply the commit hash itself
+    for commit_num in (0..=current_head).rev() {
+        // This is the header, which is simply the commit number itself
         logs.push_str(&utils::terminal_format(
-            &format!("commit {head}\n"),
+            &format!("commit {commit_num}\n"),
             utils::TerminalFormatting {
                 color: Some(utils::Color::Yellow),
                 bold: true
             }
         ));
 
-        // We retrieve the metadata from the commit file, not the contents
-        let head_metadata = fs::read_to_string(format!("{RAT_NEST}/commits/{head}"))?;
+        // We retrieve the message string from the .message file
+        let message = fs::read_to_string(format!("{RAT_NEST}/commit-{commit_num}/.message"))?;
 
-        // We need to keep track of whether we're currently reading the
-        // key/value metadata, or the commit message itself.
-        let mut capturing_message = false;
-        for metadata_line in head_metadata.lines() {
-            // If we're currently in the message portion, we should indent the
-            // message line, put it directly in our output, and move on to the
-            // next one.
-            if capturing_message {
-                logs.push_str(&(" ".repeat(4) + metadata_line));
-                continue;
-            }
+        // We append the message to our logs
+        logs.push_str(&message);
 
-            // Our simple format is defined by key/value metadata separated
-            // by a space. We split on it and match on the results.
-            let (key, value) = metadata_line.split_once(' ').unwrap_or_default();
-
-            match key {
-                "parent" => {
-                    // If this commit has a parent, that's the next commit we
-                    // have to log, so set it as our current head. If not, we've
-                    // reached the root.
-                    current_head = if !value.trim().is_empty() {
-                        Some(value.to_string())
-                    } else {
-                        None
-                    }
-                }
-                _ => capturing_message = true,
-            }
-        }
 
         // We check this condition down here because we don't want these
         // separators to be printed after the last commit.
-        if current_head.is_some() {
+        if commit_num != 0 {
             logs.push_str("\n\n");
         }
     }
--- src/0.2-logging/src/utils.rs
+++ src/0.2-logging/src/utils.rs
@@ -4,12 +4,9 @@
 //! May use slightly more advanced Rust concepts. If you're primarily trying to
 //! learn about git, it's not necessary to attempt to read and understand these.
 
-use std::error::Error;
 use std::fs;
 use std::io;
 use std::path::Path;
-
-use sha2::Digest;
 
 /// Recursively copies the contents, including subdirectories, of `from` into
 /// `to`. Ignores any paths that match those contained in the `ignore` array.
@@ -53,49 +50,42 @@ pub fn copy_dir_deep(
     Ok(())
 }
 
-/// Updates `hasher` with the contents of all files in the given directory
-/// recursively, ignoring paths that match those in `ignore`.
-pub fn hash_directory(
-    hasher: &mut impl Digest,
-    directory: impl AsRef<Path>,
-    ignore: &[impl AsRef<Path>],
-) -> Result<(), Box<dyn Error>> {
-    // This is very similiar to copy_dir_deep, see there for details
-    // We could generalize these methods in theory, but for simplicity purposes
-    // we are not going to do that.
-
-    let dir_entries = fs::read_dir(&directory)?
-        .collect::<Result<Vec<_>, _>>()?;
-
-    // Directory entries have no guaranteed order, so in order to be able to
-    // hash the same files consistently, we need to sort them first.
-    dir_entries.sort_by_key(|entry| entry.file_name());
-
-    for dir_entry in dir_entries {
-        let entry_name = dir_entry.file_name();
-
-        if ignore
-            .iter()
-            .any(|ignore_path| ignore_path.as_ref() == entry_name)
-        {
-            continue;
-        }
-
-        if dir_entry.file_type()?.is_file() {
-            hasher.update(fs::read_to_string(dir_entry.path())?)
-        } else {
-            hash_directory(hasher, &dir_entry.path(), ignore)?;
-        }
-    }
-
-    Ok(())
+#[non_exhaustive]
+#[derive(PartialEq, Eq, Clone, Copy)]
+pub enum Color {
+    Red,
+    Yellow,
+    Green,
 }
 
-/// Encode a byte array into a hex string for a hash
-pub fn encode_hash(byte_array: impl AsRef<[u8]>) -> String {
-    byte_array
-        .as_ref()
-        .iter()
-        .map(|byte| format!("{:02x}", byte))
-        .collect()
+#[derive(PartialEq, Eq, Clone, Copy, Default)]
+pub struct TerminalFormatting {
+    pub color: Option<Color>,
+    pub bold: bool,
+}
+
+const ANSI_ESCAPE: &str = "\u{001b}";
+const ANSI_RESET: &str = "[0m";
+
+/// Wrap a piece of text in ANSI escape codes in order to format it.
+pub fn terminal_format(text: &str, formatting: TerminalFormatting) -> String {
+    let color_code = match formatting.color {
+        Some(Color::Red) => format!("{ANSI_ESCAPE}[31m"),
+        Some(Color::Green) => format!("{ANSI_ESCAPE}[32m"),
+        Some(Color::Yellow) => format!("{ANSI_ESCAPE}[33m"),
+        None => "".to_string(),
+    };
+
+    let highlighting = if formatting.bold {
+        format!("{ANSI_ESCAPE}[1m")
+    } else {
+        "".to_string()
+    };
+
+    format!(
+        "{color_code}\
+        {highlighting}\
+        {text}\
+        {ANSI_ESCAPE}{ANSI_RESET}"
+    )
 }
